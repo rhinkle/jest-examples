3d67675e7970775c9e72c431e30d2f1d2f471532
Ryan Hinkle <2176012+rhinkle@users.noreply.github.com>
2024-02-28 21:04:25 -0500

    feat: added test mockChildComponent example

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -1,16 +1,18 @@
+import App from './app';
 import { render } from '@testing-library/react';
 
-import App from './app';
-import React from 'react';
+jest.mock('./fragments/todo', () => () => {
+  return <div>Todo</div>;
+});
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
-
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
     expect(getByText('Jest Testing Examples')).toBeTruthy();
+  });
+
+  it('shouod do a thing', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Todo')).toBeTruthy();
   });
 });
